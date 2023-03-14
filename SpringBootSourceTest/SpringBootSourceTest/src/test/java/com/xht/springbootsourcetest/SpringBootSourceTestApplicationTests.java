e8994428ca5332bc8f7913dabdb8d937707b0857
xiahaitao <630395100@qq.com>
2023-03-14 09:26:24 +0800

    aa

---
+++ SpringBootSourceTest/SpringBootSourceTest/src/test/java/com/xht/springbootsourcetest/SpringBootSourceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xht.springbootsourcetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSourceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
