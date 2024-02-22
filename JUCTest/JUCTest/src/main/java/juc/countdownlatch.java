8f22fc45c93839ce6c9d4dd253030c6332680f7c
xiahaitao <630395100@qq.com>
2024-02-22 21:18:35 +0800

    juc

--- JUCTest/JUCTest/src/main/java/juc/countdownlatch.java
+++ JUCTest/JUCTest/src/main/java/juc/countdownlatch.java
@@ -12,28 +12,30 @@ import java.util.concurrent.CountDownLatch;
 public class countdownlatch {
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(5);
-//        for (int i = 0; i < 5; i++) {
-//            new Thread(()->{
-//                try {
-//                    System.out.println("线程："+Thread.currentThread().getName()+"开始进入");
-//                    countDownLatch.await();
-//                    Thread.sleep(1000);
-//                    System.out.println("线程："+Thread.currentThread().getName()+"开始执行");
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }).start();
-//            countDownLatch.countDown();
-//        }
+        for (int i = 0; i < 15; i++) {
+            new Thread(()->{
+                try {
+                    System.out.println("线程："+Thread.currentThread().getName()+"开始进入");
 
-        for (int i = 0; i < 5; i++) {
-            new Thread(() -> {
-                    countDownLatch.countDown();
-                    System.out.println("线程：" + Thread.currentThread().getName() + "开始执行");
+                    countDownLatch.await();
+
+                    System.out.println("线程："+Thread.currentThread().getName()+"开始执行");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }).start();
+            Thread.sleep(2000);
+            countDownLatch.countDown();
         }
-        System.out.println("主线程--等待--");
-        countDownLatch.await();
-        System.out.println("任务完成");
+
+//        for (int i = 0; i < 5; i++) {
+//            new Thread(() -> {
+//                    countDownLatch.countDown();
+//                    System.out.println("线程：" + Thread.currentThread().getName() + "开始执行");
+//            }).start();
+//        }
+//        System.out.println("主线程--等待--");
+//        countDownLatch.await();
+//        System.out.println("任务完成");
     }
 }
---
+++ JUCTest/JUCTest/src/main/java/juc/cyclicbarrier.java
@@ -0,0 +1,58 @@
+package juc;
+
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * @ClassName: CyclicBarrier
+ * @Description: TODO
+ * @Author: xiahaitao
+ * @Date: 2024/2/22 9:39
+ * @Version: V1.0
+ */
+public class cyclicbarrier {
+
+    //保存每个学生的平均成绩
+    private ConcurrentHashMap<String, Integer> map=new ConcurrentHashMap<String,Integer>();
+
+    private ExecutorService threadPool= Executors.newFixedThreadPool(3);
+
+    private CyclicBarrier cb=new CyclicBarrier(3,()->{
+        int result=0;
+        Set<String> set = map.keySet();
+        for(String s:set){
+            result+=map.get(s);
+        }
+        System.out.println("三人平均成绩为:"+(result/3)+"分");
+    });
+
+
+    public void count(){
+        for(int i=0;i<3;i++){
+            threadPool.execute(new Runnable(){
+
+                @Override
+                public void run() {
+                    //获取学生平均成绩
+                    int score=(int)(Math.random()*40+60);
+                    map.put(Thread.currentThread().getName(), score);
+                    System.out.println(Thread.currentThread().getName()
+                            +"同学的平均成绩为："+score);
+                    try {
+                        //执行完运行await(),等待所有学生平均成绩都计算完毕
+                        cb.await();
+                    } catch (InterruptedException | BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            });
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        cyclicbarrier cb=new cyclicbarrier();
+        cb.count();
+    }
+
+}
