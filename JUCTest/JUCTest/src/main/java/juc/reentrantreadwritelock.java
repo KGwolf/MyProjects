85395ead37a58961ac1a355bce475cf3f3a3ce35
海涛 夏 <630395100@qq.com>
2024-02-28 22:29:40 +0800

    1

---
+++ JUCTest/JUCTest/src/main/java/juc/reentrantreadwritelock.java
@@ -0,0 +1,13 @@
+package juc;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @Author: xiahaitao
+ * @CreateTime: 2024-02-28  21:18
+ * @Description: TODO
+ * @Version: 1.0
+ **/
+public class reentrantreadwritelock {
+    ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+}
