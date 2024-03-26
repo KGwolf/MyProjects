776f24da1d2a4ace01ffb85bb555cbeafd02b6fc
xiahaitao <630395100@qq.com>
2024-03-26 17:29:50 +0800

    xianchengchi

--- JUCTest/JUCTest/src/main/java/juc/MyThreadPool.java
+++ JUCTest/JUCTest/src/main/java/juc/MyThreadPool.java
@@ -1,5 +1,7 @@
 package juc;
 
+import java.util.concurrent.*;
+
 /**
  * @ClassName: MyThreadPool
  * @Description: TODO
@@ -8,4 +10,9 @@ package juc;
  * @Version: V1.0
  */
 public class MyThreadPool {
+    final static ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(5,5,1, TimeUnit.MINUTES,new LinkedBlockingDeque<>());
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+    }
 }
