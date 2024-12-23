854726a13d6973b85724c15d04012608df6210ca
xiahaitao <630395100@qq.com>
2024-12-23 21:00:52 +0800

    mvc

---
+++ SpringBootSourceTest/SpringBootSourceTest/src/main/java/com/xht/springbootsourcetest/controller/MyTestController.java
@@ -0,0 +1,23 @@
+package com.xht.springbootsourcetest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @ClassName: MyTestController
+ * @Description: TODO
+ * @Author: xiahaitao
+ * @Date: 2024/12/17 19:01
+ * @Version: V1.0
+ */
+@Controller
+@RequestMapping("/mytest")
+public class MyTestController {
+
+    @RequestMapping("test1")
+    @ResponseBody
+    public String test1(){
+        return "xht";
+    }
+}
