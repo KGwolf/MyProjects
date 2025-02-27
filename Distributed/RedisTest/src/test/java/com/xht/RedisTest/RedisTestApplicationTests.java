efe37ae8b5b24383b863c224a4076393c0839db7
xiahaitao <630395100@qq.com>
2025-02-27 11:38:28 +0800

    redisTest 和 ShardingSphereTest提交

--- Distributed/RedisTest/src/test/java/com/xht/RedisTest/RedisTestApplicationTests.java
+++ Distributed/RedisTest/src/test/java/com/xht/RedisTest/RedisTestApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.redis;
+package com.xht.RedisTest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RedisTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
---
+++ Distributed/ShardingSphereTest/src/test/java/com/xht/ShardingSphereTest/ShardingSphereTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xht.ShardingSphereTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingSphereTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
