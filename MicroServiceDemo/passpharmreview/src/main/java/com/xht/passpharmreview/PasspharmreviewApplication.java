a82171fe0dae19af2fd5b4a63e73012b1924bf8e
海涛 夏 <630395100@qq.com>
2025-04-07 08:04:31 +0800

    周末提交的修改

--- MicroServiceDemo/passpharmreview/src/main/java/com/xht/passpharmreview/PasspharmreviewApplication.java
+++ MicroServiceDemo/passpharmreview/src/main/java/com/xht/passpharmreview/PasspharmreviewApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 
+/**
+ * @description: 集成sentinal(规则持久化) + skywalking + seata（at模式+TCC）
+ * @author: xiahaitao 
+ * @date: 2025/4/4 20:32
+ * @param: 
+ * @return: 
+ **/
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.xht.projectcommom")
 @Import(RedisExtConifg.class)
