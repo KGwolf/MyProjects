0d44d074a286cc532ad81baeaf911f452be250a2
海涛 夏 <630395100@qq.com>
2025-04-07 08:07:07 +0800

    TODO的问题

--- MicroServiceDemo/passpharmreview/src/main/java/com/xht/passpharmreview/PasspharmreviewApplication.java
+++ MicroServiceDemo/passpharmreview/src/main/java/com/xht/passpharmreview/PasspharmreviewApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Import;
 
 /**
  * @description: 集成sentinal(规则持久化) + skywalking + seata（at模式+TCC）
+ * TODO:sentinel超过1分钟没请求控制台上面就会消失统计信息，但规则信息不会消失
  * @author: xiahaitao 
  * @date: 2025/4/4 20:32
  * @param: 
